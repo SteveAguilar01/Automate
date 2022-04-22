d07b8eed6b99b9d38070214488523c8590baf3c8
Steve A <60375372+sja2021@users.noreply.github.com>
2022-04-21 22:09:05 -0700

    Create python_atbs1.py

---
+++ python_atbs1.py
@@ -0,0 +1,7 @@
+#This is the first lesson from Automate the boring stuff with Python
+#For now it will start simple and we will slowly build upon this while pushing to the github repo
+
+#Create a variable
+#Notice that spam is actually 42 and saying spam are different!
+spam = 42
+print('spam? or ' + str(spam))
