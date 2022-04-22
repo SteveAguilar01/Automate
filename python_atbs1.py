efd16747adc0fe383e9c73b8525f45a88fe5fa50
Steve A <60375372+sja2021@users.noreply.github.com>
2022-04-21 22:20:39 -0700

    Update python_atbs1.py
    Push 2 w/comments
    Pickle setup with question and input

--- python_atbs1.py
+++ python_atbs1.py
@@ -2,6 +2,20 @@
 #For now it will start simple and we will slowly build upon this while pushing to the github repo
 
 #Create a variable
-#Notice that spam is actually 42 and saying spam are different!
-spam = 42
-print('spam? or ' + str(spam))
+#Notice output that pickle is actually 42 and saying pickle are different!
+pickle = 42
+print('pickle? or ' + str(pickle))
+
+#Following Section 1 we will now add input
+#Now I decided to have little fun with it
+print('What is name the of your pickle?')
+rempickle = input()
+print('That is good you remember!' + rempickle)
+print('Your pickle name is quite long, it is:')
+print(len(rempickle))
+print('How many years you owned that pickle??')
+
+#Notice that we can take multiple inputs and save them each into
+#their own variables!
+agepickle = input()
+print('Your pickle age will be ' + str(int(agepickle)+ 1) + ' in a year.')
