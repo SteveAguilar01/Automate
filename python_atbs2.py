98dc0d76e36c263307d4444731ab2198c772cb46
sja2021 <60375372+sja2021@users.noreply.github.com>
2022-04-26 19:42:45 -0700

    Section 2
    Boolean logic
    If, else, and elif

---
+++ python_atbs2.py
@@ -0,0 +1,16 @@
+#This is the second section from Automate the boring stuff with Python
+#The lesson will focus on flow charts and flow control concepts
+
+#Boolean value - True or False
+#Comparsion operators == equal to, != not equal to,  > greater than,  < lesser than
+# <= lesser than or equal to, >= greather than or equal to 
+#Quick examples below gives us boolean results of True or False
+
+print(42 == 42)
+print(41 == 32)
+print(42 <= 42)
+#Notice the boolean output give from these examples
+
+#Note when comparing strings and integers are not the "same"
+
+#Boolean operators include and, or and not
